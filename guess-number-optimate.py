84fa295c315394c254572ac7165ad95a948c5d1b
JelyFishhhhhh_ _ <dlanfongtwb@gmail.com>
2023-03-29 21:47:30 +0800

    Commit

---
+++ guess-number-optimate.py
@@ -0,0 +1,64 @@
+from random import random, randrange, randint
+from math import log
+from asyncio import run
+
+async def gen_random():
+    
+    global top, goal
+
+    top = int(10**random()+randint(randrange(100, 500), randrange(1000, 1500))%1000)
+
+    print(f"The range of this game is 1 ~ {top}")
+
+    goal = int(10**random()+randint(randrange(100, 500), randrange(1000, 1500)))
+
+    print(goal)
+
+    while goal > top:
+
+        goal = int(goal%top)
+
+async def guess():
+
+    global times, inp, max_times
+
+    if inp == goal:
+
+        return 200
+    
+    elif inp > goal:
+
+        print(f"The goal is lower than {inp}...")
+        print(f"There are {max_times - times} times left.")
+        return 404
+    
+    elif inp < goal:
+        print(f"The goal is higher than {inp}...")
+        print(f"There are {max_times - times} times left.")
+        return 404
+
+
+if __name__ == "__main__":
+    
+    global times, inp, max_times
+    
+    times = 1
+    run(gen_random())
+    max_times = int(log(top, 2))+1
+
+    while times < max_times:
+
+        inp = int(input("Guess a number\n> "))
+
+        response = run(guess())
+
+        if response == 200:
+            
+            print(f"Congraduation ! The number is {goal}.")
+            print(f"You make a good job making {times} times to get the number.")
+            quit()
+        
+        times +=1
+        
+    print(f"Ohhh... The goal is {goal}.")
+    print(f"You spend {times} times to guess the number. Try it again.")
