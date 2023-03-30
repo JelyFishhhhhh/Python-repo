194cc193db00890bdcbbb21ef88c899f11004795
JelyFishhhhhh_ _ <dlanfongtwb@gmail.com>
2023-03-30 19:44:15 +0800

    Commit

--- guess-number-optimate.py
+++ guess-number-optimate.py
@@ -11,11 +11,7 @@ async def gen_goal():
 
     top = int(10**random()+randint(randrange(100, 500), randrange(1000, 1500))%1000)
 
-    print(f"The range of this game is 1 ~ {top}")
-
     goal = int(10**random()+randint(randrange(100, 500), randrange(1000, 1500)))
-
-    print(goal)
 
     while goal > top:
 
@@ -25,7 +21,11 @@ async def gen_goal():
 # 數字猜測判斷與回應
 async def guess():
 
-    global times, inp, max_times
+    global times, inp, max_times, l_range, r_range
+
+    if l_range > inp or r_range < inp:
+
+        return 400
 
     if inp == goal:
 
@@ -33,13 +33,16 @@ async def guess():
     
     elif inp > goal:
 
+        r_range = inp
         print(f"The goal is lower than {inp}...")
-        print(f"There are {max_times - times} times left.")
+        print(f"You have {max_times - times} times left.")
         return 404
     
     elif inp < goal:
+
+        l_range = inp
         print(f"The goal is higher than {inp}...")
-        print(f"There are {max_times - times} times left.")
+        print(f"You have {max_times - times} times left.")
         return 404
 
 
@@ -48,24 +51,37 @@ if __name__ == "__main__":
     
     run(gen_goal())
     
-    global times, inp, max_times
+    global times, inp, max_times, l_range, r_range, waste
+    l_range = 1
+    r_range = top
     times = 1
+    waste = 0
     max_times = int(log(top, 2))+1
 
-    # 次數內重複猜測&判斷
-    while times < max_times:
+    print(f"You have {max_times} times to reach the goal number :D")
 
-        inp = int(input("Guess a number\n> "))
+    # 次數內重複猜測&判斷
+    while times <= max_times:
+
+        inp = int(input(f"Guess a number between {l_range} ~ {r_range}\n> "))
 
         response = run(guess())
 
         if response == 200:
             
-            print(f"Congraduation ! The number is {goal}.")
+            print(f"Congraduation ! The goal number is {goal}.")
             print(f"You make a good job making {times} times to get the number.")
+            print(f"(include {waste} waste times)")
             quit()
         
+        elif response == 400:
+
+            waste += 1
+            print(f"You waste one chance to reach the goal :(")
+            print(f"You have {max_times - times} times left.")
+
         times +=1
         
     print(f"Ohhh... The goal is {goal}.")
-    print(f"You spend {times} times to guess the number. Try it again.")
+    print(f"You spend {times-1} times to guess the number. Try it again.")
+    print(f"(include {waste} waste times)")
