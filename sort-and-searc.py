1e418ce38faa74e016a49aa3dfced0362a5933aa
JelyFishhhhhh <tony0940419@gmail.com>
2023-05-24 08:42:02 +0800

    Commit

--- sort-and-searc.py
+++ sort-and-searc.py
@@ -1,49 +1,55 @@
-# global
-
-val={}
-
+from asyncio import run
 # sort func_
 
-def sort():
+async def sort():
     for x in range(int(len(val))-1):
         for y in range(x+1,int(len(val))):
             # print(y)
             if val[x]>val[y]:
-                tmp=val[x]
-                val[x]=val[y]
-                val[y]=tmp
+                val[x], val[y] = val[y], val[x]
     print(val)
 
 #search func_
 
-def search(g, l, r):
-    mid=int((l+r)/2)
+async def search(g, l, r):
     
-    if l>r:
-        print('err0r')
+    mid = int((l+r)/2)
+    
+    if l > r:
+        
+        print('404 Not Founded')
         return
     
-    elif val[mid] is g:
+    elif val[mid] == g:
+        
         print(f'index:{mid}')
         return
         
-    elif val[mid]<g:
-        search(g, mid+1, r)
+    elif val[mid] < g:
+        
+        await search(g, mid+1, r)
         
     else:
-        search(g, l, mid-1)
+        
+        await search(g, l, mid-1)
 
 # main
-n=int(input('input how many numbers in the array?\n> '))
-print('input')
-for i in range(n):
-    val[i]=int(input(f'{i+1}. '))
-
-sort()
-
-m=int(input('input how many numbers need to search?\n> '))
-print('input')
-
-for i in range(m):
-    tmp=int(input(f'{m}. '))
-    search(tmp, 0, int(len(val))-1)
+if __name__ == "__main__":
+    
+    global val
+    val = {}
+    n=int(input('input how many numbers in the array?\n> '))
+    print('*input*')
+    for i in range(n):
+        val[i]=int(input(f'{i+1}. '))
+    
+    run(sort())
+    
+    m=int(input('input how many numbers need to search?\n> '))
+    print('*input*')
+    
+    for i in range(m):
+        tmp=int(input(f'{i+1}. '))
+        run(search(tmp, 0, int(len(val))))
+        
+    print("Process Ended.")
